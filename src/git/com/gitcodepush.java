82cef0fd2936780e0edcae89954f1a72a61f2562
Mahesh Kapilavai <kapilavaimaheshkumar@gmail.com>
2021-03-08 12:04:50 +0530

    first commit

---
+++ src/git/com/gitcodepush.java
@@ -0,0 +1,9 @@
+package git.com;
+
+public class gitcodepush {
+	public static void main(String[] args) {
+		System.out.println("java code push ton github");
+	
+}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module Javagithubpush {
+}
